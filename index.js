72c7b4e769f275a8725ddc039ea99e9a1390b8b8
1337 <lal@mail.ru>
2017-09-10 20:57:25 +0300

    fix anus-burn media query  bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function checkRadio(id) {
   console.log(id);
   $('html, body').animate({
-        scrollTop: $("#formSection").offset().top
+        scrollTop: $("form").offset().top
     }, 400);
   $("input[name='name']").focus();
   document.getElementById(id).checked = true;
